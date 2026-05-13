e1fd7da492429e699f46e894c15a2cfd276a17ef
Max Escriva <max.escriva-aguado@capgemini.com>
2026-05-13 16:44:32 +0200

    build: list call, connection with backend

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,14 +1,18 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Category } from './model/category';
 import { CATEGORY_DATA } from './model/mock-categories';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({ providedIn: 'root' })
 export class CategoryService {
   constructor() {}
+  protected readonly http = inject(HttpClient);
+
+  private baseUrl = 'http://localhost:8080/category';
 
   getCategories(): Observable<Category[]> {
-    return of(CATEGORY_DATA);
+    return this.http.get<Category[]>(this.baseUrl);
   }
 
   saveCategory(category: Category): Observable<Category> {
