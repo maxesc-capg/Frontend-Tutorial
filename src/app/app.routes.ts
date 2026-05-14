89417651ad4f32b5cc63fe633661b0878d04263c
Max Escriva <max.escriva-aguado@capgemini.com>
2026-05-14 13:49:51 +0200

    refactor (elements): add game model and path in app.routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+    { path: '', redirectTo: '/games', pathMatch: 'full'}, // Si no se pone ruta, por defecto redirige a games
     {
         path: 'categories',
         loadComponent: () => import('../category/category-list/category-list.page')
             .then(m => m.CategoryListComponent)
     },
     { path: 'authors', loadComponent: () => import('../author/author-list/author-list.page').then(m => m.AuthorListComponent)},
+    { path: 'games', loadComponent: () => import('../game/game-list/game-list.page').then(m => m.GameListPage)}
 ];
