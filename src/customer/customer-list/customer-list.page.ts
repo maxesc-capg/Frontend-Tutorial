40cd4c603a2c9a135787509fb1ef7a56f489ea7c
Max Escriva <max.escriva-aguado@capgemini.com>
2026-05-15 13:21:39 +0200

    feat (customer): customer list

--- src/customer/customer-list/customer-list.page.ts
+++ src/customer/customer-list/customer-list.page.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { Customer } from '../model/Customer';
+import { CommonModule } from '@angular/common';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-customer-list',
-  imports: [],
+  imports: [MatButtonModule,
+        MatIconModule,
+        MatTableModule,
+        CommonModule],
   templateUrl: './customer-list.page.html',
   styleUrl: './customer-list.page.scss',
 })
-export class CustomerListPage {}
+export class CustomerListPage implements OnInit {
+  dataSource = new MatTableDataSource<Customer>();
+  displayedColumns: string[] = ['id', 'name', 'action'];
+
+  constructor() {}
+
+  ngOnInit(): void {
+    
+  }
+
+}
